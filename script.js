7cd75eace002640b4309e716083220e66a2a402a
Sukhanpreet <sukhanpreet356@gmail.com>
2022-08-19 21:29:15 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("script working")
+
+let cardNumber=document.querySelector('#c-number')
+let cardName=document.querySelector('#c-name');
+let cardCVV=document.querySelector('#cvv')
+let cardMM=document.querySelector('#exp-mm')
+let cardYY=document.querySelector('#exp-yy')
+
+
+console.log(cardName.innerHTML);
+console.log(cardNumber.innerHTML);
+console.log(cardCVV.innerHTML);
+console.log(cardMM.innerHTML);
+console.log(cardYY.innerHTML);
+
